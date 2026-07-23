252e8657c65c18203b1016d6b44b71a0d762a4cb
Isaac "Izo" Njoroge <selfmgnt@gmail.com>
2026-07-23 21:31:40 +0300

    Disable unsupported architectures in parts.nix
    Comment out unsupported system architectures.

--- NixOS_Working - 07-2026/myNixos/modules/parts.nix
+++ NixOS_Working - 07-2026/myNixos/modules/parts.nix
@@ -2,9 +2,9 @@
   config = {
     systems = [
       "x86_64-linux"
-      "x86_64-darwin"
-      "aarch64-linux"
-      "aarch64-darwin"
+      #"x86_64-darwin"
+      #"aarch64-linux"
+      #"aarch64-darwin"
     ];
   };
 }
